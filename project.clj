0be631466c34ee143837e43edf3d9c6afc895185
Andrew Oberstar <ajoberstar@gmail.com>
2015-02-14 13:51:11 -0600

    Updating license.

--- project.clj
+++ project.clj
@@ -1,8 +1,8 @@
 (defproject ranked-choice "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
-  :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :description "Web app for ranked choice voting."
+  :url "https://github.com/ajoberstar/ranked-choice"
+  :license {:name "Apache License, Version 2.0"
+            :url "http://www.apache.org/licenses/LICENSE-2.0"}
   :dependencies [[org.clojure/clojure "1.6.0"]]
   :main ^:skip-aot ranked-choice.core
   :target-path "target/%s"
