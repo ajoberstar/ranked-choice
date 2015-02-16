fb18a5f304ba8afd31a1462f4d2f131ca0f12196
Andrew Oberstar <ajoberstar@gmail.com>
2015-02-15 21:24:16 -0600

    Adding a monitor page.

--- src/ranked_choice/routes.clj
+++ src/ranked_choice/routes.clj
@@ -7,14 +7,20 @@
             [ring.middleware.params :refer [wrap-params]]
             [net.cgrand.enlive-html :as html]
             [clojure.core.async :as async]
-            [clojure.data.json :as json]))
+            [clojure.data.json :as json]
+            [clojure.string :as string]))
 
 (html/deftemplate vote "vote.html" [id candidates]
   [:li.candidate] (html/clone-for [candidate candidates] (html/content candidate))
   [:#abstain-btn] (html/set-attr :href (str "/poll/" id "/results") ))
 
+(html/deftemplate monitor "monitor.html" [polls-by-id]
+  [:div.poll] (html/clone-for [[id poll] polls-by-id]
+                             [:.candidates] (->> poll :candidates (string/join ", ") html/content)
+                             [:a] (html/set-attr :href (str "/poll/" id "/close"))))
+
 (defn poll-routes
-  [poll id]
+  [poll-mgr poll id]
   (routes
     (GET "/vote" []
          (vote id (:candidates poll)))
@@ -30,7 +36,9 @@
              (websocket/websocket-handler in-ch out-ch))
            (resource-response "/results.html")))
     (GET "/close" []
-         (async/close! (:poll-ch poll)))))
+         (async/close! (:poll-ch poll))
+         (swap! (:polls poll-mgr) assoc (Integer/parseInt id) nil)
+         (redirect "/poll/monitor"))))
 
 (defroutes app-routes
   (GET "/" []
@@ -41,9 +49,13 @@
         (let [candidates-coll (if (instance? String candidates) [candidates] candidates)
               poll-id (voting/new-poll poll-mgr candidates-coll)]
           (redirect (str "/poll/" poll-id "/vote"))))
+  (GET "/poll/monitor" {poll-mgr :voting/poll-mgr}
+       (let [polls @(:polls poll-mgr)
+             polls-by-id (->> polls (map-indexed vector) (filter (comp :candidates last)))]
+         (monitor polls-by-id)))
   (context "/poll/:id" [id :as {poll-mgr :voting/poll-mgr}]
            (if-let [poll (voting/get-poll poll-mgr (Integer/parseInt id))]
-             (poll-routes poll id)
+             (poll-routes poll-mgr poll id)
              (not-found (str "No poll found with id: " id))))
   (resources ""))
 
