0e8c60dd8d191892ed5aa960916802286a0b0c63
Andrew Oberstar <ajoberstar@gmail.com>
2015-02-22 11:54:04 -0600

    Adding docstring.

--- src/ranked_choice/voting.clj
+++ src/ranked_choice/voting.clj
@@ -1,6 +1,8 @@
 (ns ranked-choice.voting)
 
 (defprotocol VotingSystem
+  "Protocol specifying how to
+  determine a winner of a poll."
   (count-votes [vsys votes]))
 
 (defn- format-results
@@ -13,6 +15,28 @@
        (into [])))
 
 (defn results
+  "Calculates the results of a series of votes using
+  the given VotingSystem vsys. If the votes are left
+  off, will return a function that takes votes.
+
+  Votes must be in the format of a vector with exactly
+  one occurrence of each candidate in order of the
+  voter's preference.
+
+  Results will be formatted into a vector of vectors,
+  where the inner vector consists of the candidate's
+  name followed by their total votes in each round of
+  counting. The outer vector will be sorted by the
+  ranking of the candidates (using the highest number
+  of votes in the last round first, and using previous
+  rounds as tie-breakers, as needed).
+
+  Example Results:
+
+  [[Susy 2 3 4]
+   [Billy 2 2 3]
+   [Johnny 2 2 0]
+   [Lucy 1 0 0]]"
   ([vsys]
     (comp format-results
           (partial count-votes vsys)))
