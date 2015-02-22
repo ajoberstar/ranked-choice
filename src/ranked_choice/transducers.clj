fe11ba1c947b02682f3ca3b84923f28f1e846df3
Andrew Oberstar <ajoberstar@gmail.com>
2015-02-22 10:21:30 -0600

    Adding docstrings and switching reductions to use volatile.

--- src/ranked_choice/transducers.clj
+++ src/ranked_choice/transducers.clj
@@ -2,6 +2,13 @@
   (:refer-clojure :exclude [peek reductions]))
 
 (defn peek
+  "Returns a transducer allowing visibility into input as it
+  proceeds through the transducer stack. Takes a
+  function f, which presumably has side-effects.
+
+  For each input received by the returned transducer
+  (f input) will be called before proceeding with the
+  nested transducer."
   [f]
   (fn [xf]
     (completing
@@ -10,11 +17,19 @@
         (xf result input)))))
 
 (defn reductions
+  "Returns a transducer that behaves similarly to clojure.core/reductions
+  by applying the reducing function rf to the accumulated result and the
+  new input. The result of callilng rf will be passed to the next transducer
+  in the stack.
+
+  If no init value is provided, this will behave is if you called
+  (reductions rf (rf)). That is, (rf) will be executed eagerly before
+  returning the transducer."
   ([rf]
     (reductions rf (rf)))
   ([rf init]
     (fn [xf]
-      (let [acc (atom init)]
+      (let [acc (volatile! init)]
         (completing
           (fn [result input]
-            (->> input (swap! acc rf) (xf result))))))))
+            (->> input (vswap! acc rf) (xf result))))))))
