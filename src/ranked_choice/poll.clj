3473cf6f74c2ed60ba6959cb40568398b2b6d59a
Andrew Oberstar <ajoberstar@gmail.com>
2015-02-22 11:40:34 -0600

    Adding docstrings and making PollManager idempotent.

--- src/ranked_choice/poll.clj
+++ src/ranked_choice/poll.clj
@@ -10,6 +10,12 @@
                  latest-results])
 
 (defn new-poll
+  "Creates a new poll using the provided VotingSystem
+  vsys and set of candidates. After creation voters
+  can cast votes by putting them on the :votes-ch
+  of the poll. Callers interested in the results of
+  the poll should use pipe-results to have the results
+  added to their channel. Returns the new poll."
   [vsys candidates]
   (let [latest-results (atom [])
         results-xf (comp (xf/reductions conj)
@@ -20,26 +26,40 @@
     (->Poll candidates votes-ch results-mult latest-results)))
 
 (defn pipe-results
+  "Pipes the results of the given poll onto the
+  channel ch. The latest results are always provided
+  first. Subsequent results will be provided as additional
+  votes are cast. Returns the provided channel."
   [poll ch]
   (async/put! ch @(:latest-results poll))
   (async/tap (:results-mult poll) ch)
   ch)
 
+(def ^:private close-all!
+  (comp (map :votes-ch)
+        (filter identity)
+        (map async/close!)))
+
 (defrecord PollManager [polls]
   component/Lifecycle
   (start [component]
-    (assoc component :polls (atom [])))
+    (if (:polls component)
+      component
+      (assoc component :polls (atom []))))
   (stop [component]
-    (swap! (:polls component)
-           (fn [polls]
-             (doseq [poll (keep identity polls)]
-               (async/close! (:votes-ch poll)))))
-    (dissoc component :polls)))
+    (if (:polls component)
+      (do
+        (swap! (:polls component)
+               #(into [] close-all! %))
+        (dissoc component :polls))
+      component)))
 
 (defn get-poll
+  "Gets a poll from the provided manager by id."
   [poll-mgr poll-id]
   (-> poll-mgr :polls deref (nth poll-id nil)))
 
 (defn conj-poll
+  "Conj's a poll onto the manager and returns its id."
   [poll-mgr poll]
   (-> poll-mgr :polls (swap! conj poll) count dec))
