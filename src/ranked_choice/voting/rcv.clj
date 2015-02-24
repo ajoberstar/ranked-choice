0e39fbaedb94cd739f9fc4ba9fc6fcc0bc027be6
Andrew Oberstar <ajoberstar@gmail.com>
2015-02-23 20:52:36 -0600

    New shared rcv impl is working.

--- src/ranked_choice/voting/rcv.clj
+++ src/ranked_choice/voting/rcv.clj
@@ -1,63 +1,93 @@
 (ns ranked-choice.voting.rcv
   (:require [ranked-choice.voting :as voting]
-            [clojure.core.async :as async]))
+            [clojure.core.async :as async]
+            [ranked-choice.transducers :as xf]))
+
+(defn tally-xf [tally]
+  (map (fn [ballots]
+         (->> ballots
+              (map tally)
+              (apply merge-with +)
+              (assoc {:ballots ballots} :tally)))))
 
 (defrecord RankedChoice
-  [tally winner? runoff]
+  [tally no-winner? runoff]
   voting/VotingSystem
   (count-ballots [vsys candidates ballots]
-    (let [{:keys [tally no-winner? runoff]} vsys
-          votes-ch (async/chan 1 (map tally))
-          runoff-ch (async/chan 1 (comp (take-while no-winner?)
-                                        (map runoff)))
-          results-ch (async/chan 1 (comp (map :tally)))
+    (let [base-results (zipmap candidates (repeat []))
+          base-tally (zipmap candidates (repeat 0))
+          {:keys [tally no-winner? runoff]} vsys
+          votes-ch (async/chan 1 (comp (xf/peek (partial println "Votes In: "))
+                                       (tally-xf tally)
+                                       (xf/peek (partial println "Votes Out: "))))
+          runoff-ch (async/chan 1 (comp (xf/peek (partial println "Runoff In: "))
+                                        (take-while no-winner?)
+                                        (xf/peek (partial println "Runoff Mid: "))
+                                        (map runoff)
+                                        (xf/peek (partial println "Runoff Out: "))))
+          results-ch (async/chan 1 (comp (xf/peek (partial println "Results In: "))
+                                         (map :tally)
+                                         (map #(into base-tally %))
+                                         (xf/peek (partial println "Results Out: "))))
           tally-mult (async/mult votes-ch)]
       (async/tap tally-mult runoff-ch)
       (async/tap tally-mult results-ch)
       (async/pipe runoff-ch votes-ch)
       (async/put! votes-ch ballots)
-      (async/into [] results-ch))))
+      (async/reduce #(merge-with conj %1 %2)
+                    base-results
+                    results-ch))))
 
-(defn- first-count [vote] {(first vote) 1})
+(defn- first-count [ballot] {(first ballot) 1})
+
 (defn- bucklin-count [_] nil)
-(defn- borda-count [_] nil)
-(defn- nauru-count [_] nil)
+
+(defn- borda-count [ballot]
+  (->> (range)
+       (drop 1)
+       (zipmap (reverse ballot))))
+
+(defn- nauru-count [ballot]
+  (->> (range)
+       (drop 1)
+       (map #(/ 1 %))
+       (zipmap ballot)))
 
 (defn- no-majority?
   [{:keys [tally]}]
   (let [totals (map last tally)
         votes-needed (/ (reduce + totals) 2)]
-    (some #(> % votes-needed) totals)))
+    (every? #(< % votes-needed) totals)))
 
 (defn- challenger?
-  [{:keys [tally]}]
-  (->> tally (map last) (filter pos?) (count) (= 1)))
+  [{:keys [ballots]}]
+  (some #(->> % count (< 1)) ballots))
 
 (def ^:private one-round (constantly false))
 
 (defn- runoff-below
-  [votes tally threshold]
+  [ballots tally threshold]
   (let [pred (comp #(<= % threshold) last)
         losers (->> tally (filter pred) (map first) (into #{}))]
-    (map (partial remove losers) votes)))
+    (map (partial remove losers) ballots)))
 
-(defn- runoff-but-last
-  [{:keys [tally votes]}]
-  (->> tally (map last) (reduce min) (runoff-below votes tally)))
+(defn runoff-but-last
+  [{:keys [tally ballots]}]
+  (->> tally (map last) (reduce min) (runoff-below ballots tally)))
 
 (defn- runoff-avg
-  [{:keys [tally votes]}]
+  [{:keys [tally ballots]}]
   (let [totals (map last tally)
         avg (/ (reduce + totals) (count totals))]
-    (runoff-below votes tally avg)))
+    (runoff-below ballots tally avg)))
 
 (defn- runoff-coombs [_] nil)
 
-(defn- runoff-everyone [{:keys [votes]}] votes)
+(defn- runoff-everyone [{:keys [ballots]}] ballots)
 
 (def irv (->RankedChoice first-count no-majority? runoff-but-last))
-(def coombs (->RankedChoice first-count no-majority? runoff-coombs))
-(def bucklin (->RankedChoice bucklin-count no-majority? runoff-everyone))
+;; (def coombs (->RankedChoice first-count no-majority? runoff-coombs))
+;; (def bucklin (->RankedChoice bucklin-count no-majority? runoff-everyone))
 (def borda (->RankedChoice borda-count one-round runoff-everyone))
 (def nauru (->RankedChoice nauru-count one-round runoff-everyone))
 (def nanson (->RankedChoice borda-count challenger? runoff-avg))
