ee121a314cc8189e9d87a8672aed2b8b4392b9ce
Andrew Oberstar <ajoberstar@gmail.com>
2015-02-24 19:07:25 -0600

    Fixing java-agent for newrelic.

--- project.clj
+++ project.clj
@@ -17,11 +17,11 @@
                  [http-kit "2.1.19"]
                  [compojure "1.3.2"]
                  [enlive "1.1.5"]]
+  :java-agents [[com.newrelic.agent.java/newrelic-agent "3.9.0"]]
   :main ^:skip-aot ranked-choice.core
   :target-path "target/%s"
   :uberjar-name "ranked-choice.jar"
   :profiles {:uberjar {:aot :all}
-             :java-agents [[com.newrelic.agent.java/newrelic-agent "3.9.0"]]
              :dev {:dependencies [[reloaded.repl "0.1.0"]
                                   [org.clojure/tools.namespace "0.2.9"]]
                    :source-paths ["dev"]}})
