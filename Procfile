ef82cc42fa6d30899fbbda1c3a96755d3a31461b
Andrew Oberstar <ajoberstar@gmail.com>
2018-05-19 17:51:18 -0500

    Convert to Gradle
    Still need to switch code to use new deps.

--- Procfile
+++
@@ -1,1 +0,0 @@
-web: java -jar target/uberjar/ranked-choice.jar $PORT
--- test/ranked_choice/core_test.clj
+++
@@ -1,7 +0,0 @@
-(ns ranked-choice.core-test
-  (:require [clojure.test :refer :all]
-            [ranked-choice.core :refer :all]))
-
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
