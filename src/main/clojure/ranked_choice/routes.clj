c4bf99d2ab8ea2fa73c06740c32dfe074488f0db
Andrew Oberstar <ajoberstar@gmail.com>
2018-05-20 22:15:08 -0500

    basic api

--- src/main/clojure/ranked_choice/routes.clj
+++ src/main/clojure/ranked_choice/routes.clj
@@ -1,8 +1,11 @@
 (ns ranked-choice.routes
-  (:require [bidi.ring :as bidi]
+  (:require [bidi.bidi :as bidi]
+            [bidi.ring :refer [make-handler]]
             [ring.util.response :as resp]
             [ring.middleware.defaults :refer [wrap-defaults site-defaults]]
             [ring.middleware.json :refer [wrap-json-response]]))
+
+(declare route)
 
 (defn ok [request]
   (resp/response {:status "OK"}))
@@ -10,11 +13,21 @@
 (defn index [_]
   (resp/redirect "index.html"))
 
+(defn poll-view [request]
+  (resp/response ["Calvin" "Hobbes" "Locke"]))
+
+(defn polls-list [request]
+  (->> [123 456 789]
+       (map #(bidi/path-for route poll-view :id %))
+       (into [])
+       (resp/response)))
+
 (def route ["/" {"" index
-                 "api/" ok}])
+                 "api/poll/" {"" polls-list
+                              [:id] poll-view}}])
 
 (def site
   (-> route
-      (bidi/make-handler)
+      (make-handler)
       (wrap-json-response)
       (wrap-defaults site-defaults)))
