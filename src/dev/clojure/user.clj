a4fd8ac83fcede8f65e2241326dafe9003647bfe
Andrew Oberstar <ajoberstar@gmail.com>
2018-06-23 09:54:26 -0500

    Use latest gradle-clojure

--- src/dev/clojure/user.clj
+++ src/dev/clojure/user.clj
@@ -1,6 +1,7 @@
 (ns user
   (:require [mount.core :as mount]
-            [clojure.tools.namespace.repl :as tn]))
+            [clojure.tools.namespace.repl :as tn]
+            [gradle-clojure.tools.figwheel :as fw]))
 
 (defn dev []
   (in-ns 'dev))
---
+++ src/dev/clojurescript/ranked_choice/dev.cljs
@@ -0,0 +1,5 @@
+(ns ranked-choice.dev)
+
+(enable-console-print!)
+
+(println "It's dev time!")
