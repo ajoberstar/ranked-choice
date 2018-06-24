b0bf50876b52f42afccfadac58a58a2a7939fcb9
Andrew Oberstar <ajoberstar@gmail.com>
2018-06-24 17:38:40 -0500

    With parser

--- src/main/clojurescript/ranked_choice/frontend/main.cljs
+++ src/main/clojurescript/ranked_choice/frontend/main.cljs
@@ -5,27 +5,33 @@
 
 (def app-state (atom {:count 0}))
 
+(defn read [{:keys [state] :as env} key params]
+  (let [st @state]
+    (if-let [[_ v] (find st key)]
+      {:value v}
+      {:value :not-found})))
+
+(defn mutate [{:keys [state] :as env} key params]
+  (if (= 'increment key)
+    {:value {:keys [:count]}
+     :action #(swap! state update-in [:count] inc)}
+    {:value :not-found}))
+
 (defui Counter
+  static om/IQuery
+  (query [this]
+         [:count])
   Object
   (render [this]
           (let [{:keys [count]} (om/props this)]
             (dom/div nil
                      (dom/span nil (str "Count: " count))
                      (dom/button #js {:onClick (fn [e]
-                                                 (swap! app-state update-in [:count] inc))}
+                                                 (om/transact! this '[(increment)]))}
                                  "Click me!")))))
 
-(def reconciler (om/reconciler {:state app-state}))
+(def reconciler (om/reconciler {:state app-state
+                                :parser (om/parser {:read read :mutate mutate})}))
 
 (om/add-root! reconciler
               Counter (gdom/getElement "app"))
-
-; (defui HelloWorld
-;   Object
-;   (render [this]
-;           (let [{:keys [title]} (om/props this)]
-;             (dom/h1 nil title))))
-
-; (def hello-world (om/factory HelloWorld))
-
-; (js/ReactDOM.render (apply dom/div nil (map #(hello-world {:react-key % :title (str "Hi, " % " times")}) (range 10))) (gdom/getElement "app"))
