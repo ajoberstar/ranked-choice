a933b066c1d8613130f593c4378f78746c05a3bc
Andrew Oberstar <ajoberstar@gmail.com>
2018-06-24 18:21:48 -0500

    With normalization

--- src/main/clojurescript/ranked_choice/frontend/main.cljs
+++ src/main/clojurescript/ranked_choice/frontend/main.cljs
@@ -1,37 +1,108 @@
 (ns ranked-choice.frontend.main
   (:require [om.next :as om :refer-macros [defui]]
             [om.dom :as dom]
-            [goog.dom :as gdom]))
+            [goog.dom :as gdom]
+            [clojure.pprint :as pp]))
 
-(def app-state (atom {:count 0}))
+(def init-data
+  {:list/one [{:name "John" :points 0}
+              {:name "Mary" :points 0}
+              {:name "Bob"  :points 0}]
+   :list/two [{:name "Mary" :points 0 :age 27}
+              {:name "Gwen" :points 0}
+              {:name "Jeff" :points 0}]})
 
-(defn read [{:keys [state] :as env} key params]
+(defmulti read om/dispatch)
+
+(defn get-people [state key]
   (let [st @state]
-    (if-let [[_ v] (find st key)]
-      {:value v}
-      {:value :not-found})))
+    (into [] (map #(get-in st %)) (get st key))))
 
-(defn mutate [{:keys [state] :as env} key params]
-  (if (= 'increment key)
-    {:value {:keys [:count]}
-     :action #(swap! state update-in [:count] inc)}
-    {:value :not-found}))
+(defmethod read :list/one
+  [{:keys [state] :as env} key params]
+  {:value (get-people state key)})
 
-(defui Counter
+(defmethod read :list/two
+  [{:keys [state] :as env} key params]
+  {:value (get-people state key)})
+
+(defmulti mutate om/dispatch)
+
+(defmethod mutate 'points/increment
+  [{:keys [state]} _ {:keys [name]}]
+  {:action
+   (fn []
+     (swap! state update-in
+            [:person/by-name name :points]
+            inc))})
+
+(defmethod mutate 'points/decrement
+  [{:keys [state]} _ {:keys [name]}]
+  {:action
+   (fn []
+     (swap! state update-in
+            [:person/by-name name :points]
+            #(let [n (dec %)] (if (neg? n) 0 n))))})
+
+(def parser (om/parser {:read read :mutate mutate}))
+
+(defui Person
+  static om/Ident
+  (ident [this {:keys [name]}]
+         [:person/by-name name])
   static om/IQuery
   (query [this]
-         [:count])
+         '[:name :points :age])
   Object
   (render [this]
-          (let [{:keys [count]} (om/props this)]
-            (dom/div nil
-                     (dom/span nil (str "Count: " count))
-                     (dom/button #js {:onClick (fn [e]
-                                                 (om/transact! this '[(increment)]))}
-                                 "Click me!")))))
+          (println "Render Person" (-> this om/props :name))
+          (let [{:keys [points name] :as props} (om/props this)]
+            (dom/li nil
+                    (dom/label nil (str name ", points: " points))
+                    (dom/button
+                     #js {:onClick
+                          (fn [e]
+                            (om/transact! this
+                                          `[(points/increment ~props)]))}
+                     "+")
+                    (dom/button
+                     #js {:onClick
+                          (fn [e]
+                            (om/transact! this
+                                          `[(points/decrement ~props)]))}
+                     "-")))))
 
-(def reconciler (om/reconciler {:state app-state
-                                :parser (om/parser {:read read :mutate mutate})}))
+(def person (om/factory Person {:keyfn :name}))
+
+(defui ListView
+  Object
+  (render [this]
+          (println "Render ListView" (-> this om/path first))
+          (let [list (om/props this)]
+            (apply dom/ul nil
+                   (map person list)))))
+
+(def list-view (om/factory ListView))
+
+(defui RootView
+  static om/IQuery
+  (query [this]
+         (let [subquery (om/get-query Person)]
+           `[{:list/one ~subquery} {:list/two ~subquery}]))
+  Object
+  (render [this]
+          (println "Render RootView")
+          (let [{:keys [list/one list/two]} (om/props this)]
+            (apply dom/div nil
+                   [(dom/h2 nil "List A")
+                    (list-view one)
+                    (dom/h2 nil "List B")
+                    (list-view two)]))))
+
+(def reconciler
+  (om/reconciler
+   {:state  init-data
+    :parser (om/parser {:read read :mutate mutate})}))
 
 (om/add-root! reconciler
-              Counter (gdom/getElement "app"))
+              RootView (gdom/getElement "app"))
