2f46195d7783062cab538e3e81effa1237182a1b
Andrew Oberstar <ajoberstar@gmail.com>
2018-06-25 21:48:02 -0500

    Use new gradle-clojure
    Fixes some devcards issues

--- src/dev/clojurescript/ranked_choice/frontend/cards.cljs
+++ src/dev/clojurescript/ranked_choice/frontend/cards.cljs
@@ -2,8 +2,22 @@
   (:require [goog.dom :as gdom]
             [om.next :as om :refer-macros [defui]]
             [om.dom :as dom]
-            [devcards.core :as dc :refer-macros [defcard defcard-om-next defcard-doc start-devcard-ui!]]))
+            [devcards.core :as dc :refer-macros [defcard start-devcard-ui!]]))
 
+(start-devcard-ui!)
 
-(defcard other-stuff
-  "Look at you")
+(defcard
+  "## This is markdown!
+
+ It is the **greatest** `markup` language!.
+ ")
+
+(defui Hello
+  Object
+  (render [this]
+          (dom/p nil (str "Hello, " (-> this om/props :name) "!"))))
+
+(def hello (om/factory Hello))
+
+(defcard simple-hello
+  (hello {:name "Andy"}))
--- src/main/clojurescript/ranked_choice/frontend/main.cljs
+++ src/main/clojurescript/ranked_choice/frontend/main.cljs
@@ -1,24 +1,4 @@
 (ns ranked-choice.frontend.main
   (:require [goog.dom :as gdom]
             [om.next :as om :refer-macros [defui]]
-            [om.dom :as dom]
-            [devcards.core :as dc :refer-macros [defcard defcard-om-next defcard-doc start-devcard-ui!]]
-            [ranked-choice.frontend.cards]))
-
-(start-devcard-ui!)
-
-(defcard
-  "## This is markdown!
-
- It is the **greatest** `markup` language!.
- ")
-
-(defui Hello
-  Object
-  (render [this]
-          (dom/p nil (str "Hello, " (-> this om/props :name) "!"))))
-
-(def hello (om/factory Hello))
-
-(defcard simple-hello
-  (hello {:name "Andy"}))
+            [om.dom :as dom]))
